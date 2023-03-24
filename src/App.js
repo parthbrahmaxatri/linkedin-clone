af3a6afd1c4bdf46defc0bd4f4bf56e7f63f93c0
Parth Brahmaxatri <parthbrahmaxatri007@gmail.com>
2023-03-24 11:22:29 +0530

    Create login component and add it to router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h2>Let's build the linkedin clone🚀</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
